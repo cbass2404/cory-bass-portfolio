6d0a7f2b3b23c14f0cfd8780b982d8ef950abf81
Cory Bass <cory.bass.07@gmail.com>
2021-07-20 15:38:28 -0500

    refactored /posts/[slug]

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,14 +1,16 @@
 import { Fragment } from 'react';
 import Head from 'next/dist/next-server/lib/head';
-import { getSession } from 'next-auth/client';
+import { useSession } from 'next-auth/client';
 
 import CommentSection from '../../components/comments/CommentSection';
 import PostDetail from '../../components/posts/postDetails/PostDetail';
-import { getPostDetails } from '../../lib/posts';
+import { getPostDetails, getPostsFiles } from '../../lib/posts';
 import { getPostComments } from '../../lib/comments';
 
 const BlogDetailPage = (props: any) => {
-  const { post, session, comments } = props;
+  const { post, comments } = props;
+
+  const [session, loading] = useSession();
 
   if (post) {
     return (
@@ -34,21 +36,34 @@ const BlogDetailPage = (props: any) => {
   );
 };
 
-export const getServerSideProps = async (context: any) => {
+export const getStaticProps = async (context: any) => {
   const { params } = context;
   const slug = params.slug;
 
   const postDetails = getPostDetails(slug);
-  const postComments = getPostComments(slug);
-
-  const session = await getSession({ req: context.req });
+  const postComments = await getPostComments(slug);
 
   return {
     props: {
       post: postDetails,
-      session: session,
       comments: postComments,
     },
+    revalidate: 60,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  const pathsWithParams = slugs.map((slug) => ({
+    params: { slug },
+  }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: false,
   };
 };
 
